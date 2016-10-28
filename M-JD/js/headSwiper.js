8ecc2a298e9185cc4de71c214d665153093af169
董建强 <18513552894@163.com>
2016-10-28 10:22:27 +0800

    部分demo包
    董建强

---
+++ M-JD/js/headSwiper.js
@@ -0,0 +1,9 @@
+window.onload=function () {
+	var swiper = new Swiper('.swiper-container', {
+	pagination: '.swiper-pagination',
+	paginationClickable: true,
+	loop: true,
+	autoplay:2500,
+	autoplayDisableOnInteraction: false
+	});
+}
